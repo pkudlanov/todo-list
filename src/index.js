33a14006c94e41be97b4e1a5ec71ea6ece55e443
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-20 18:12:55 -0700

    added dynamic header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import listTemplate from './list-template.js';
 import htmlToDOM from './html-to-DOM.js';
 import todo from './data.js';
+import headerTemplate from './header-template.js';
 
 const list = document.getElementById('todo-list');
+const header = document.getElementById('header');
 
 todo.forEach(listItem => {
     const html = listTemplate(listItem);
@@ -11,3 +13,7 @@ todo.forEach(listItem => {
 
     list.appendChild(dom);
 });
+
+const html = headerTemplate();
+const dom = htmlToDOM(html);
+header.appendChild(dom);
