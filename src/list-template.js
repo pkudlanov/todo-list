b8e8d31b4f909b3975a443550c776518824405b4
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-20 17:45:00 -0700

    both tests passing and everything right

--- src/list-template.js
+++ src/list-template.js
@@ -1,9 +1,12 @@
-import todo from "./data";
-
 function listTemplate(todo) {
+    let checked = '';
+
+    if(todo.completed) {
+        checked = ' checked';
+    }
     return /*html*/ `
         <li class="list-item">
-            <p><input class="checkbox" type="checkbox">${todo.task}</p>
+            <p><input class="checkbox" type="checkbox"${checked}>${todo.task}</p>
         </li>
     `;
 }
