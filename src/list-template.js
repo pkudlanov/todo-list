3c7a33d0ea969042e5c219ba7466f530ad260fd9
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-20 17:39:01 -0700

    passing test for false completed

--- src/list-template.js
+++ src/list-template.js
@@ -1,5 +1,9 @@
 function listTemplate() {
-
+    return /*html*/ `
+        <li class="list-item">
+            <p><input class="checkbox" type="checkbox">Pack my stuff</p>
+        </li>
+    `;
 }
 
 export default listTemplate;
