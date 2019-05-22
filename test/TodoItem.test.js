01b202136a79ead6b6805283d3747bbe7275a91f
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-22 11:02:06 -0700

    updated previous todo template test to work for TodoItem

---
+++ test/TodoItem.test.js
@@ -0,0 +1,46 @@
+import TodoItem from '../src/components/TodoItem.js';
+
+const test = QUnit.test;
+
+QUnit.module('list item');
+
+test('list item for false', (assert) => {
+    // arrange
+    const todo = {
+        task: 'Pack my stuff',
+        completed: false
+    };
+
+    const expected = /*html*/`
+        <li class="list-item">
+            <p><input class="checkbox" type="checkbox">Pack my stuff</p>
+        </li>
+    `;
+
+    // act
+    const todoItem = new TodoItem({ todo });
+    const html = todoItem.renderTemplate();
+
+    // assert
+    assert.htmlEqual(html, expected);
+});
+
+// test('list item for true', (assert) => {
+//     // arrange
+//     const todo = {
+//         task: 'Pack my stuff',
+//         completed: true
+//     };
+
+//     const expected = /*html*/`
+//         <li class="list-item">
+//             <p><input class="checkbox" type="checkbox" checked>Pack my stuff</p>
+//         </li>
+//     `;
+
+//     // act
+//     const html = listTemplate(todo);
+
+//     // assert
+//     assert.htmlEqual(html, expected);
+// });
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,3 @@
 import './html-equal.js';
 
-import './list-template.test.js';
-import './header-template.test.js';
+import './TodoItem.test.js';
