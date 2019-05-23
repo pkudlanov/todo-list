25cd85f8692bc182e12d641b051560524bb85b9e
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-23 14:58:47 -0700

    made the todo list longer for better filter testing

--- data/todo-list.js
+++ data/todo-list.js
@@ -20,9 +20,73 @@ const todos = [
         completed: false
     },
     {
-        task: 'Go HOME!!!!!!!',
+        task: 'Get off the train',
         completed: false
     },
+    {
+        task: 'Find my car',
+        completed: false
+    },
+    {
+        task: 'Find my keys',
+        completed: false
+    },
+    {
+        task: 'Find the door lock',
+        completed: false
+    },
+    {
+        task: 'Oh wait I have remote unlock',
+        completed: false
+    },
+    {
+        task: 'Unlock the vehicle',
+        completed: false
+    },
+    {
+        task: 'Throw my stuff in',
+        completed: false
+    },
+    {
+        task: 'Throw myself in',
+        completed: false
+    },
+    {
+        task: 'Find the ignition',
+        completed: false
+    },
+    {
+        task: 'Ignite the engine',
+        completed: false
+    },
+    {
+        task: 'Put the car in day mode, night mode doesn\'t work',
+        completed: false
+    },
+    {
+        task: 'Drive out of the parking lot',
+        completed: false
+    },
+    {
+        task: 'Make a left turn',
+        completed: false
+    },
+    {
+        task: 'Go to the end',
+        completed: false
+    },
+    {
+        task: 'Make another left turn',
+        completed: false
+    },
+    {
+        task: 'Then the first right turn',
+        completed: false
+    },
+    {
+        task: 'The house is the second on the left',
+        completed: false
+    }
 ];
 
 export default todos;
